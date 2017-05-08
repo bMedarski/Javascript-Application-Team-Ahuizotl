0deb9f449eaf4ac2f3de1d60ae4cc028d0cc3cf3
KPKirilov <k.p.kirilov@abv.bg>
2017-05-08 21:01:49 +0300

    Added username and password validation
    valid usernames: 4-20 in length only letters and nubers
    valid passwords: 6-30 in length only letters and nubers
    NOTE: Validator.isValidUsername() only checks if it meets the above
    criteria, it does not check if a same username already exists,

--- scripts/users-manager.js
+++ scripts/users-manager.js
@@ -10,7 +10,8 @@ Kinvey.initialize({
 var dataStore = Kinvey.DataStore.collection('FollowedTeams');
 
 const UsersManager =  {
-        register: function(username, password, successCallback, failCallback) {
+    register: function(username, password, successCallback, failCallback) {
+        if (Validator.isValidUsername(username) && Validator.isValidPassword(password)) {
             UsersManager.logout(function(){
                 var user = new Kinvey.User();
                 var promise = user.signup({
@@ -23,10 +24,14 @@ const UsersManager =  {
                     failCallback();
                 });
             });
- 
-        },
+        } 
+        else {
+            failCallback();
+        }
+    },
 
-        login: function(username, password, successCallback, failCallback) {
+    login: function(username, password, successCallback, failCallback) {
+        if (Validator.isValidUsername(username) && Validator.isValidPassword(password)) {
             UsersManager.logout(function(){
                 var user = new Kinvey.User();
                 var promise = user.login({
@@ -39,84 +44,87 @@ const UsersManager =  {
                     failCallback();
                 }); 
             });
-        },
-
-        logout: function(callback) {
-            var promise = Kinvey.User.logout();
-            promsie = promise.then(function onSuccess() {
-                localStorage.setItem('username', null);
-                callback();
-            }).catch(function onError(error) {                
-                callback();
-            });
-        },
-
-        getFollowedTeams: function(callback) {
-            var query = new Kinvey.Query();
-            query.equalTo('username', localStorage.getItem('username'));
-            var stream = dataStore.find(query);
-            var entities = [];
-            stream.subscribe(function onNext(e) {
-                entities = e; 
-            }, function onError(error) {
-                // ...
-            }, function onComplete() {
-                try {
-                    callback(entities[0].teams);
-                } 
-                catch(e) {
-                    var emptyTeamsArray = [];
-                    callback(emptyTeamsArray);
-                }
-            });            
-        },
-
-
-        followTeam: function(teamName) {
-            UsersManager.getFollowedTeams(function(teamsArray) {
-                if(teamsArray.indexOf(teamName) < 0) {
-                    UsersManager.unfollowAllTeams(function(){
-                        teamsArray.push(teamName);
-                        var promise = dataStore.save({
-                        username: localStorage.getItem('username'),
-                        teams: teamsArray
-                        }).then(function onSuccess(entity) {
-                            //console.log('team followed SUCC')
-                        }).catch(function onError(error) {
-                            //console.log('team followed FAILED')
-                        });
-                    });
-                }
-            });
-        },
-
-        unfollowTeam: function(teamName) {
-            UsersManager.getFollowedTeams(function(teamsArray) {
-                var index = teamsArray.indexOf(teamName);
-                if(index >= 0) {
-                    UsersManager.unfollowAllTeams(function(){
-                        teamsArray.splice(index, 1);
-                        var promise = dataStore.save({
-                        username: localStorage.getItem('username'),
-                        teams: teamsArray
-                        }).then(function onSuccess(entity) {
-                            //console.log('team unfollowed SUCC')
-                        }).catch(function onError(error) {
-                            //console.log('team unfollowed FAILED')
-                        });
-                    });
-                }
-            });
-        },
-
-        unfollowAllTeams: function(callback) {
-            var query = new Kinvey.Query();
-            query.equalTo('username', localStorage.getItem('username'));
-            var promise = dataStore.remove(query)
-            promise = promise.then(function onSuccess(result) {
-                callback();
-            }).catch(function onError(error) {
-                // ...
-            });
         }
+        else {
+            failCallback();
+        }
+    },
+
+    logout: function(callback) {
+        var promise = Kinvey.User.logout();
+        promsie = promise.then(function onSuccess() {
+            localStorage.setItem('username', null);
+            callback();
+        }).catch(function onError(error) {                
+            callback();
+        });
+    },
+
+    getFollowedTeams: function(callback) {
+        var query = new Kinvey.Query();
+        query.equalTo('username', localStorage.getItem('username'));
+        var stream = dataStore.find(query);
+        var entities = [];
+        stream.subscribe(function onNext(e) {
+            entities = e; 
+        }, function onError(error) {
+            // ...
+        }, function onComplete() {
+            try {
+                callback(entities[0].teams);
+            } 
+            catch(e) {
+                var emptyTeamsArray = [];
+                callback(emptyTeamsArray);
+            }
+        });            
+    },
+
+    followTeam: function(teamName) {
+        UsersManager.getFollowedTeams(function(teamsArray) {
+            if(teamsArray.indexOf(teamName) < 0) {
+                UsersManager.unfollowAllTeams(function(){
+                    teamsArray.push(teamName);
+                    var promise = dataStore.save({
+                    username: localStorage.getItem('username'),
+                    teams: teamsArray
+                    }).then(function onSuccess(entity) {
+                        //console.log('team followed SUCC')
+                    }).catch(function onError(error) {
+                        //console.log('team followed FAILED')
+                    });
+                });
+            }
+        });
+    },
+
+    unfollowTeam: function(teamName) {
+        UsersManager.getFollowedTeams(function(teamsArray) {
+            var index = teamsArray.indexOf(teamName);
+            if(index >= 0) {
+                UsersManager.unfollowAllTeams(function(){
+                    teamsArray.splice(index, 1);
+                    var promise = dataStore.save({
+                    username: localStorage.getItem('username'),
+                    teams: teamsArray
+                    }).then(function onSuccess(entity) {
+                        //console.log('team unfollowed SUCC')
+                    }).catch(function onError(error) {
+                        //console.log('team unfollowed FAILED')
+                    });
+                });
+            }
+        });
+    },
+
+    unfollowAllTeams: function(callback) {
+        var query = new Kinvey.Query();
+        query.equalTo('username', localStorage.getItem('username'));
+        var promise = dataStore.remove(query)
+        promise = promise.then(function onSuccess(result) {
+            callback();
+        }).catch(function onError(error) {
+            // ...
+        });
+    }
 } 
